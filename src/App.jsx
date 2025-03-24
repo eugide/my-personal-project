737d85c49e87689d8576879446e550939a9691b4
eugide <eugidendungutse770@gmail.com>
2025-03-24 11:39:45 +0200

    feat: Add footer in portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Footer from "./components/Fototer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
+      
       
     </>
   );
